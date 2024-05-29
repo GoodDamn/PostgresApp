98450ffbee5542854a8ac5911ada5f281f351a0d
GoodDamn <grigorydum@gmail.com>
2024-05-29 09:30:14 +0300

    Fix users.js

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,21 @@
 var express = require('express');
+var session = require('../session.js');
 var router = express.Router();
 
 router.get('/', async function(req, res, next) {
     let user = session.auth(req).user;
+    
+    if (user == undefined || user == null) {
+	res.status(401).send('Session does not exist');
+	return;
+    }
+	
     if (user.role === 'employee') {
 	res.status(403).send('Access denied');
 	return;
     }
 	
-    res.render('users/list', { title: 'Пользователи' })
-
+    res.render('users/list', { title: 'Пользователи' });	
 });
 
 
